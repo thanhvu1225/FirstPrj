111b3d0ad20d5538d973dc7666245a7dbfb68f2b
Nguyễn Vũ Thành <178261903+thanhvu1225@users.noreply.github.com>
2024-08-15 22:23:14 +0700

    Add files via upload

---
+++ Wellcome.java
@@ -0,0 +1,6 @@
+public class Wellcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyen Vu Thanh!");
+        System.out.println("Wellcome to the DHKTPM19A!");
+    }
+}
